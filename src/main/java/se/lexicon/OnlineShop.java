69b99042716c9058390a882ef9ce8bdae12e1e18
Dan Lundberg <dan-lundberg@outlook.com>
2026-06-30 10:01:20 +0200

    Add basic try-catch for the main loop responding to IllegalArgumentException. .

--- src/main/java/se/lexicon/OnlineShop.java
+++ src/main/java/se/lexicon/OnlineShop.java
@@ -2,37 +2,43 @@ package se.lexicon;
 
 public class OnlineShop {
     static void main() {
-        // Creating two products
-        Product coffee = new Product("Coffee", 25.0);
-        Product sandwich = new Product("Sandwich", 65.0);
+        try {
+            // Creating two products
+            Product coffee = new Product("Coffee", 25.0);
+            Product sandwich = new Product("Sandwich", 65.0);
 
-        // Creating a customer named Joe
-        Customer joe = new Customer("Joe Smith", "joe.smith@test.com");
+            // Creating a customer named Joe
+            Customer joe = new Customer("Joe Smith", "joe.smith@test.com");
 
-        // Create an order for Joe
-        Order joesOrder = new Order(joe);
-        joesOrder.addProduct(coffee);
-        joesOrder.addProduct(sandwich);
+            // Create an order for Joe
+            Order joesOrder = new Order(joe);
+            joesOrder.addProduct(coffee);
+            joesOrder.addProduct(sandwich);
 
-        // Print the order summary for Joe
-        joesOrder.printSummary();
+            // Print the order summary for Joe
+            joesOrder.printSummary();
 
-        // Remove one order form the list
-        joesOrder.removeProduct(coffee);
+            // Remove one order form the list
+            joesOrder.removeProduct(coffee);
 
-        // Print the order summary for Joe again
-        joesOrder.printSummary();
+            // Print the order summary for Joe again
+            joesOrder.printSummary();
 
-        // Create another product
-        Product iceCream = new Product("Ice cream", 35.0);
+            // Create another product
+            Product iceCream = new Product("Ice cream", 35.0);
 
-        // Apply a small discount on the Ice Cream, since it's summer... ;)
-        iceCream.applyDiscount(10);
+            // Apply a small discount on the Ice Cream, since it's summer... ;)
+            iceCream.applyDiscount(10);
 
-        // Add Ice Cream to Joe's order
-        joesOrder.addProduct(iceCream);
+            // Add Ice Cream to Joe's order
+            joesOrder.addProduct(iceCream);
 
-        // Print the order summary for Joe again
-        joesOrder.printSummary();
+            // Print the order summary for Joe again
+            joesOrder.printSummary();
+        } catch (IllegalArgumentException iae) {
+            IO.println(iae.getMessage());
+        } catch (Exception e) {
+            IO.println("Something went wrong. See error message: " + e.getMessage());
+        }
     }
 }
